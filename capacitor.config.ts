46dd0c99fb5bfb9e5151fe13557391f4f14276e4
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-03-29 16:04:29 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'R0334-3009-assignment-4-demo-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
