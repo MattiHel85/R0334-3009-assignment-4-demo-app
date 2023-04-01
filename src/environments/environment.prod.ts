6b0550297bddd7f823808365bd6bdccec82104d4
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-04-01 20:00:54 +0300

    Push finished app to GitHub

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'assignment-4-demo-app',
+    appId: '1:1065003794711:web:0538f96cc2b49ad7083752',
+    storageBucket: 'assignment-4-demo-app.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBvmZetGRxUAirumFCmW_Rg0WlOy_tn4nc',
+    authDomain: 'assignment-4-demo-app.firebaseapp.com',
+    messagingSenderId: '1065003794711',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'assignment-4-demo-app',
+    appId: '1:1065003794711:web:0538f96cc2b49ad7083752',
+    storageBucket: 'assignment-4-demo-app.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBvmZetGRxUAirumFCmW_Rg0WlOy_tn4nc',
+    authDomain: 'assignment-4-demo-app.firebaseapp.com',
+    messagingSenderId: '1065003794711',
+  },
   production: false
 };
 
